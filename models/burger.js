e7dbb316a20dae81008244f0b8f478711f0b7195
Gigi Tannous <gtannous16@gmail.com>
2019-05-29 18:28:22 -0400

    added the burger js code

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+var orm = require('../config/orm.js');
+
+//create meal
+var meal = {
+    //select all meal entries
+    selectAll: function(cb) {
+        orm.selectAll('meals',function(res){
+            cb(res);
+        });
+    },
+    // variables cols & vals are arrays
+    insertOne: function(cols,vals, cb){
+        orm.insertOne('meals', cols, vals, function(res){
+            cb(res);
+        });
+    },
+    //objColVals is an object which specifies columns as object keys with values that are assoiated
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne('meals',objColVals, condition, function(res){
+            cb(res);
+        });
+    }
+};
+
+//export th database functios for the burgercontroller.js
+
+module.exports = burger;
