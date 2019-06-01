f8eea6afb86f0fd605640c9ddc2e0ff489e38f54
Gigi Tannous <gtannous16@gmail.com>
2019-06-01 13:17:36 -0400

    added id to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO meals (meal_name, devoured) VALUES ('Chicken Tenders & Fries', false);
-INSERT INTO meals (meal_name, devoured) VALUES ('Cheeseburger & Fries', false);
-INSERT INTO meals (meal_name, devoured) VALUES ('Pepperoni Pizza & Side Salad', false);
-INSERT INTO meals (meal_name, devoured) VALUES ('Fettuccine Alfredo & Ceasar Salad', false);
-INSERT INTO meals (meal_name, devoured) VALUES ('Filet Mignon & Mashed Potatoes', false);
-INSERT INTO meals (meal_name, devoured) VALUES ('Enchilidas with Rice & Black Beans', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (1,'Chicken Tenders & Fries', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (2,'Cheeseburger & Fries', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (3,'Pepperoni Pizza & Side Salad', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (4,'Fettuccine Alfredo & Ceasar Salad', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (5,'Filet Mignon & Mashed Potatoes', false);
+INSERT INTO meals (id, meal_name, devoured) VALUES (6,'Enchilidas with Rice & Black Beans', false);
